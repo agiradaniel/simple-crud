d299f76a61c085a6a4f147665fe051d1bc43d49a
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2023-01-31 21:27:05 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const mysql2 = require('mysql2');
 const cors = require('cors')
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    user: 'root',
+//connecting to the database
+const db = mysql2.createConnection({
+    user: 'agiradaniel',
     host: 'localhost',
-    password: 'karanjadanfever9223',
-    database: 'employeeSystem'
+    password: '9223',
+    database: 'employeeSystem',
 });
 
+//sending data to the database
 app.post('/create', (req, res) =>{
     const name = req.body.name;
     const age = req.body.age;
@@ -20,15 +22,48 @@ app.post('/create', (req, res) =>{
     const position = req.body.position;
     const wage = req.body.wage;
 
-    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)',
+    db.query('INSERT INTO alure (name, age, country, position, wage) VALUES (?,?,?,?,?)',
      [name, age, country, position, wage]), (err, result) => {
         if(err){
-            console.log(err)
+            console.log(err);
         }else{
             res.send('Values inserted');
         }
      }
 
+})
+
+app.get('/employees', (req,res) => {
+    db.query('SELECT * FROM alure', (err, result) =>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    })
+})
+
+app.put('/update', (req,res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query('UPDATE alure SET wage = ? WHERE id = ?', [wage, id], (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    });
+})
+
+app.delete('/delete/:id', (req,res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM alure WHERE id = ?', id, (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    })
 })
 
 app.listen(3001, ()=> {
